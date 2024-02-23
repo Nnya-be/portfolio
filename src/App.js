2a803fde6604dc2c8fb257a24c17b2fb360e2f85
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-02-23 01:35:57 +0000

    Added: the components into the app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import "./App.css";
 import "./styles/styles.scss";
 import Home from "./pages/Home";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import { Testimonials } from "./components/Testimonials";
+
 function App() {
   return (
-  <div className="App">
-        <Navigation></Navigation>
-        <Home></Home>
-        <Skills></Skills>
-  </div>);
+    <div className="App">
+      <Navigation></Navigation>
+      <Home></Home>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Testimonials></Testimonials>
+    </div>
+  );
 }
 
 export default App;
